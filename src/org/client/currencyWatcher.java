ba71d0c1922516980df483e281f219cc076ea748
kareem <kareem.elshahawe@gmail.com>
2016-05-14 09:28:01 +0800

    Finsihing up the client side code.
    - Client side code is finished, I need to add the styling and refactoring.
    - Next: Styling, adding percentage change.
    - Next: write the currency parsers and link server to client side.

--- src/org/client/currencyWatcher.java
+++ src/org/client/currencyWatcher.java
@@ -1,147 +1,85 @@
 package org.client;
 
-import org.shared.FieldVerifier;
+import java.util.Date;
+
+import org.client.customWidgets.AddCurrencyBtn;
+import org.client.customWidgets.CurrencyTable;
+import org.client.customWidgets.CurrencyTxtBox;
+import org.client.customWidgets.CustomCurrencyTable;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class currencyWatcher implements EntryPoint {
-  /**
-   * The message displayed to the user when the server cannot be reached or
-   * returns an error.
-   */
-  private static final String SERVER_ERROR = "An error occurred while "
-      + "attempting to contact the server. Please check your network "
-      + "connection and try again.";
+	private VerticalPanel mainPanel = new VerticalPanel();
+	private CurrencyTable currencyTable = new CustomCurrencyTable();
+	private HorizontalPanel addCurrencyPanel = new HorizontalPanel();
+	private Label lastUpdateLbl = new Label();
+	private CurrencyTxtBox currencyTxtBx = new CurrencyTxtBox(currencyTable);
+	private AddCurrencyBtn addCurrBtn = new AddCurrencyBtn("Add", currencyTable, currencyTxtBx);
 
-  /**
-   * Create a remote service proxy to talk to the server-side Greeting service.
-   */
-  private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
+	/**
+	 * The message displayed to the user when the server cannot be reached or
+	 * returns an error.
+	 */
+	private static final String SERVER_ERROR = "An error occurred while "
+			+ "attempting to contact the server. Please check your network " + "connection and try again.";
 
-  /**
-   * This is the entry point method.
-   */
-  public void onModuleLoad() {
-    final Button sendButton = new Button("Send");
-    final TextBox nameField = new TextBox();
-    nameField.setText("GWT User");
-    final Label errorLabel = new Label();
+	/**
+	 * Create a remote service proxy to talk to the server-side Greeting
+	 * service.
+	 */
+	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
 
-    // We can add style names to widgets
-    sendButton.addStyleName("sendButton");
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		initAddCurrPanel();
+		initMainPanel();
+		initRootPanel();
+		currencyTxtBx.setFocus(true);
+		Timer t = new Timer() {
 
-    // Add the nameField and sendButton to the RootPanel
-    // Use RootPanel.get() to get the entire body element
-    RootPanel.get("nameFieldContainer").add(nameField);
-    RootPanel.get("sendButtonContainer").add(sendButton);
-    RootPanel.get("errorLabelContainer").add(errorLabel);
+			@Override
+			public void run() {
+				currencyTable.refreshPriceLists();
+				refreshLastUpdateLbl();
+			}
+		};
+		t.scheduleRepeating(700);
+	}
 
-    // Focus the cursor on the name field when the app loads
-    nameField.setFocus(true);
-    nameField.selectAll();
+	private void initAddCurrPanel() {
+		this.addCurrencyPanel.add(addCurrBtn);
+		this.addCurrencyPanel.add(this.currencyTxtBx);
+	}
 
-    // Create the popup dialog box
-    final DialogBox dialogBox = new DialogBox();
-    dialogBox.setText("Remote Procedure Call");
-    dialogBox.setAnimationEnabled(true);
-    final Button closeButton = new Button("Close");
-    // We can set the id of a widget by accessing its Element
-    closeButton.getElement().setId("closeButton");
-    final Label textToServerLabel = new Label();
-    final HTML serverResponseLabel = new HTML();
-    VerticalPanel dialogVPanel = new VerticalPanel();
-    dialogVPanel.addStyleName("dialogVPanel");
-    dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-    dialogVPanel.add(textToServerLabel);
-    dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-    dialogVPanel.add(serverResponseLabel);
-    dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-    dialogVPanel.add(closeButton);
-    dialogBox.setWidget(dialogVPanel);
+	private void initMainPanel() {
+		this.mainPanel.add((Widget) this.currencyTable);
+		this.mainPanel.add(this.addCurrencyPanel);
+		this.mainPanel.add(this.lastUpdateLbl);
+	}
 
-    // Add a handler to close the DialogBox
-    closeButton.addClickHandler(new ClickHandler() {
-      public void onClick(ClickEvent event) {
-        dialogBox.hide();
-        sendButton.setEnabled(true);
-        sendButton.setFocus(true);
-      }
-    });
+	private void initRootPanel() {
+		RootPanel.get("currencyList").add(this.mainPanel);
+	}
+	private void refreshLastUpdateLbl(){
+		// Display timestamp showing last refresh.
+	      DateTimeFormat dateFormat = DateTimeFormat.getFormat(
+	        DateTimeFormat.PredefinedFormat.DATE_TIME_MEDIUM);
+	      lastUpdateLbl.setText("Last update : " 
+	        + dateFormat.format(new Date()));
+	}
 
-    // Create a handler for the sendButton and nameField
-    class MyHandler implements ClickHandler, KeyUpHandler {
-      /**
-       * Fired when the user clicks on the sendButton.
-       */
-      public void onClick(ClickEvent event) {
-        sendNameToServer();
-      }
-
-      /**
-       * Fired when the user types in the nameField.
-       */
-      public void onKeyUp(KeyUpEvent event) {
-        if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-          sendNameToServer();
-        }
-      }
-
-      /**
-       * Send the name from the nameField to the server and wait for a response.
-       */
-      private void sendNameToServer() {
-        // First, we validate the input.
-        errorLabel.setText("");
-        String textToServer = nameField.getText();
-        if (!FieldVerifier.isValidName(textToServer)) {
-          errorLabel.setText("Please enter at least four characters");
-          return;
-        }
-        
-        // Then, we send the input to the server.
-        sendButton.setEnabled(false);
-        textToServerLabel.setText(textToServer);
-        serverResponseLabel.setText("");
-        greetingService.greetServer(textToServer, new AsyncCallback<String>() {
-          public void onFailure(Throwable caught) {
-            // Show the RPC error message to the user
-            dialogBox.setText("Remote Procedure Call - Failure");
-            serverResponseLabel.addStyleName("serverResponseLabelError");
-            serverResponseLabel.setHTML(SERVER_ERROR);
-            dialogBox.center();
-            closeButton.setFocus(true);
-          }
-
-          public void onSuccess(String result) {
-            dialogBox.setText("Remote Procedure Call");
-            serverResponseLabel.removeStyleName("serverResponseLabelError");
-            serverResponseLabel.setHTML(result);
-            dialogBox.center();
-            closeButton.setFocus(true);
-          }
-        });
-      }
-    }
-
-    // Add a handler to send the name to the server
-    MyHandler handler = new MyHandler();
-    sendButton.addClickHandler(handler);
-    nameField.addKeyUpHandler(handler);
-  }
 }
